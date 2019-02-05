fc85bc312c005940e841e03aef58b54cbf6600fc
John Haythorn <johnhaythorn@yahoo.co.uk>
2019-02-05 14:18:02 +0000

    fizzbuzz refactored

--- jasmine/jasmine-standalone-3.3.0/spec/fizzBuzzSpec.js
+++ jasmine/jasmine-standalone-3.3.0/spec/fizzBuzzSpec.js
@@ -1,14 +1,22 @@
-describe("fizzBuzz", function() {
+describe("FizzBuzz", function() {
+
+  let fizzBuzz;
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  }
+);
+
   it("Will say fizz if the number can be divided from 3", function() {
-    expect(fizzbuzz(3)).toEqual("fizz");
+    expect(fizzBuzz.numbers(3)).toEqual("fizz");
   });
   it("Will say buzz if the number can be divided from 5", function() {
-    expect(fizzbuzz(5)).toEqual("buzz");
+    expect(fizzBuzz.numbers(5)).toEqual("buzz");
   });
   it("Will say fizzbuzz if the number can be divided from 5 and 3", function() {
-    expect(fizzbuzz(15)).toEqual("fizzbuzz");
+    expect(fizzBuzz.numbers(15)).toEqual("fizzbuzz");
   });
   it("Will return the number if is not divisible by 3 or 5", function() {
-    expect(fizzbuzz(1)).toEqual(1);
+    expect(fizzBuzz.numbers(1)).toEqual(1);
   });
 });
--- jasmine/jasmine-standalone-3.3.0/src/fizzBuzz.js
+++ jasmine/jasmine-standalone-3.3.0/src/fizzBuzz.js
@@ -1,14 +1,15 @@
-function fizzbuzz(number){
-  if (number%3===0 && number%5===0){
-    return "fizzbuzz"
-  }
-  else if (number%5===0) {
-    return "buzz"
-  }
-  else if (number%3===0) {
-    return "fizz"
-  }
-  else {
-      return number
-    }
+function FizzBuzz() {
+};
+
+FizzBuzz.prototype.numbers = function(number) {
+    if (number %15===0){return "fizzbuzz";}
+    if (number %5===0) {return "buzz";}
+    if (number%3===0) {return "fizz";}
+    return number;
+};
+
+let fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i<=100; i++) {
+  console.log(fizzBuzz.numbers(i));
 };
